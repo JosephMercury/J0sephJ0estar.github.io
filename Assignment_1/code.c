44bda42b995bec8eedf12af7cea361e768a0eabf
J0sephJ0estar <544893870@qq.com>
2017-04-01 14:59:32 +0800

    Update code.c

--- Assignment_1/code.c
+++ Assignment_1/code.c
@@ -1,19 +1,18 @@
-#include<windows.h>//一定要加在第一行
+#include<windows.h>//涓€瀹氳鍔犲湪绗竴琛屼笉鐒朵細鎶ラ敊
 #include <GL/glut.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include "unistd.h"
 #include <stdio.h>
-//接收Esc
+//鎺ユ敹Esc
 #define ESCAPE 27
 
-/* The number of our GLUT window */
 int window;
 
-//定义画图中的各个参数
+//瀹氫箟鐢诲浘涓殑鍚勪釜鍙傛暟
 float angleOfLeftArmA,angleOfLeftArmB,angleOfRightArmA,angleOfRightArmB,angleOfLeftLegA,angleOfLeftLegB,angleOfRightLegA,angleOfRightLegB,a = 0.4,b=0.2,angle = 0.1;
 GLuint texture[1];
-//openGL初始化程序
+//openGL鍒濆鍖栫▼搴?
 void InitGL(int Width, int Height)
 {
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
@@ -43,39 +42,39 @@ void ReSizeGLScene(int Width, int Height)
     glMatrixMode(GL_MODELVIEW);
 }
 
-//画身体的各个部分的函数
-//openGL左边系为右手系且y轴朝上，z轴从屏幕指出
-//一次画两边，因为是根据y轴对称所以每对都是y上坐标不变
+//鐢昏韩浣撶殑鍚勪釜閮ㄥ垎鐨勫嚱鏁?
+//openGL宸﹁竟绯讳负鍙虫墜绯讳笖y杞存湞涓婏紝z杞翠粠灞忓箷鎸囧嚭
+//涓€娆＄敾涓よ竟锛屽洜涓烘槸鏍规嵁y杞村绉版墍浠ユ瘡瀵归兘鏄痽涓婂潗鏍囦笉鍙?
 void DrawBody()
 {
     glColor3f(0.0f,1.0f,1.0f);
     glBegin(GL_QUADS);
-    //画朝前的面
+    //鐢绘湞鍓嶇殑闈?
     glVertex3f(-0.5f, 1.0f, 0.25f);
     glVertex3f( 0.5f, 1.0f, 0.25f);
     glVertex3f( 0.5f,-1.0f, 0.25f);
     glVertex3f(-0.5f,-1.0f, 0.25f);
-    //画朝左的面
+    //鐢绘湞宸︾殑闈?
     glVertex3f( 0.5f, 1.0f, 0.25f);
     glVertex3f( 0.5f, 1.0f,-0.25f);
     glVertex3f( 0.5f,-1.0f,-0.25f);
     glVertex3f( 0.5f,-1.0f, 0.25f);
-    //画朝后的面
+    //鐢绘湞鍚庣殑闈?
     glVertex3f( 0.5f, 1.0f,-0.25f);
     glVertex3f(-0.5f, 1.0f,-0.25f);
     glVertex3f(-0.5f,-1.0f,-0.25f);
     glVertex3f( 0.5f,-1.0f,-0.25f);
-    //画朝右的面
+    //鐢绘湞鍙崇殑闈?
     glVertex3f(-0.5f, 1.0f, 0.25f);
     glVertex3f(-0.5f, 1.0f,-0.25f);
     glVertex3f(-0.5f,-1.0f,-0.25f);
     glVertex3f(-0.5f,-1.0f, 0.25f);
-    //画朝上的面
+    //鐢绘湞涓婄殑闈?
     glVertex3f( 0.5f, 1.0f, 0.25f);
     glVertex3f( 0.5f, 1.0f,-0.25f);
     glVertex3f(-0.5f, 1.0f,-0.25f);
     glVertex3f(-0.5f, 1.0f, 0.25f);
-    //画朝下的面
+    //鐢绘湞涓嬬殑闈?
     glVertex3f( 0.5f,-1.0f, 0.25f);
     glVertex3f( 0.5f,-1.0f,-0.25f);
     glVertex3f(-0.5f,-1.0f,-0.25f);
@@ -86,32 +85,32 @@ void DrawShoulder()
 {
     glColor3f(0.0f,1.0f,1.0f);
     glBegin(GL_QUADS);
-    //画朝前的面
+    //鐢绘湞鍓嶇殑闈?
     glVertex3f(-1.0f, 0.2f, 0.25f);
     glVertex3f( 1.0f, 0.2f, 0.25f);
     glVertex3f( 1.0f,-0.2f, 0.25f);
     glVertex3f(-1.0f,-0.2f, 0.25f);
-    //画朝左的面
+    //鐢绘湞宸︾殑闈?
     glVertex3f( 1.0f, 0.2f, 0.25f);
     glVertex3f( 1.0f, 0.2f,-0.25f);
     glVertex3f( 1.0f,-0.2f,-0.25f);
     glVertex3f( 1.0f,-0.2f, 0.25f);
-    //画朝后的面
+    //鐢绘湞鍚庣殑闈?
     glVertex3f( 1.0f, 0.2f,-0.25f);
     glVertex3f(-1.0f, 0.2f,-0.25f);
     glVertex3f(-1.0f,-0.2f,-0.25f);
     glVertex3f( 1.0f,-0.2f,-0.25f);
-    //画朝右的面
+    //鐢绘湞鍙崇殑闈?
     glVertex3f(-1.0f, 0.2f, 0.25f);
     glVertex3f(-1.0f, 0.2f,-0.25f);
     glVertex3f(-1.0f,-0.2f,-0.25f);
     glVertex3f(-1.0f,-0.2f, 0.25f);
-    //画朝上的面
+    //鐢绘湞涓婄殑闈?
     glVertex3f( 1.0f, 0.2f, 0.25f);
     glVertex3f( 1.0f, 0.2f,-0.25f);
     glVertex3f(-1.0f, 0.2f,-0.25f);
     glVertex3f(-1.0f, 0.2f, 0.25f);
-    //画朝下的面
+    //鐢绘湞涓嬬殑闈?
     glVertex3f( 1.0f,-0.2f, 0.25f);
     glVertex3f( 1.0f,-0.2f,-0.25f);
     glVertex3f(-1.0f,-0.2f,-0.25f);
@@ -122,32 +121,32 @@ void DrawWaise()
 {
     glColor3f(0.0f,1.0f,1.0f);
     glBegin(GL_QUADS);
-    //画朝前的面
+    //鐢绘湞鍓嶇殑闈?
     glVertex3f(-0.7f, 0.2f, 0.25f);
     glVertex3f( 0.7f, 0.2f, 0.25f);
     glVertex3f( 0.7f,-0.2f, 0.25f);
     glVertex3f(-0.7f,-0.2f, 0.25f);
-    //画朝左的面
+    //鐢绘湞宸︾殑闈?
     glVertex3f( 0.7f, 0.2f, 0.25f);
     glVertex3f( 0.7f, 0.2f,-0.25f);
     glVertex3f( 0.7f,-0.2f,-0.25f);
     glVertex3f( 0.7f,-0.2f, 0.25f);
-    //画朝后的面
+    //鐢绘湞鍚庣殑闈?
     glVertex3f( 0.7f, 0.2f,-0.25f);
     glVertex3f(-0.7f, 0.2f,-0.25f);
     glVertex3f(-0.7f,-0.2f,-0.25f);
     glVertex3f( 0.7f,-0.2f,-0.25f);
-    //画朝右的面
+    //鐢绘湞鍙崇殑闈?
     glVertex3f(-0.7f, 0.2f, 0.25f);
     glVertex3f(-0.7f, 0.2f,-0.25f);
     glVertex3f(-0.7f,-0.2f,-0.25f);
     glVertex3f(-0.7f,-0.2f, 0.25f);
-    //画朝上的面
+    //鐢绘湞涓婄殑闈?
     glVertex3f( 0.7f, 0.2f, 0.25f);
     glVertex3f( 0.7f, 0.2f,-0.25f);
     glVertex3f(-0.7f, 0.2f,-0.25f);
     glVertex3f(-0.7f, 0.2f, 0.25f);
-    //画朝下的面
+    //鐢绘湞涓嬬殑闈?
     glVertex3f( 0.7f,-0.2f, 0.25f);
     glVertex3f( 0.7f,-0.2f,-0.25f);
     glVertex3f(-0.7f,-0.2f,-0.25f);
@@ -159,32 +158,32 @@ void DrawArmA()
     glColor3f(0.0f,0.0f,1.0f);
     glBegin(GL_QUADS);
 
-    //画朝前的面
+    //鐢绘湞鍓嶇殑闈?
     glVertex3f(-0.2f, 0.2f, 0.25f);
     glVertex3f( 0.2f, 0.2f, 0.25f);
     glVertex3f( 0.2f,-1.0f, 0.25f);
     glVertex3f(-0.2f,-1.0f, 0.25f);
-    //画朝左的面
+    //鐢绘湞宸︾殑闈?
     glVertex3f( 0.2f, 0.2f, 0.25f);
     glVertex3f( 0.2f, 0.2f,-0.25f);
     glVertex3f( 0.2f,-1.0f,-0.25f);
     glVertex3f( 0.2f,-1.0f, 0.25f);
-    //画朝后的面
+    //鐢绘湞鍚庣殑闈?
     glVertex3f( 0.2f, 0.2f,-0.25f);
     glVertex3f(-0.2f, 0.2f,-0.25f);
     glVertex3f(-0.2f,-1.0f,-0.25f);
     glVertex3f( 0.2f,-1.0f,-0.25f);
-    //画朝右的面
+    //鐢绘湞鍙崇殑闈?
     glVertex3f(-0.2f, 0.2f, 0.25f);
     glVertex3f(-0.2f, 0.2f,-0.25f);
     glVertex3f(-0.2f,-1.0f,-0.25f);
     glVertex3f(-0.2f,-1.0f, 0.25f);
-    //画朝上的面
+    //鐢绘湞涓婄殑闈?
     glVertex3f( 0.2f, 0.2f, 0.25f);
     glVertex3f( 0.2f, 0.2f,-0.25f);
     glVertex3f(-0.2f, 0.2f,-0.25f);
     glVertex3f(-0.2f, 0.2f, 0.25f);
-    //画朝下的面
+    //鐢绘湞涓嬬殑闈?
     glVertex3f( 0.2f,-1.0f, 0.25f);
     glVertex3f( 0.2f,-1.0f,-0.25f);
     glVertex3f(-0.2f,-1.0f,-0.25f);
@@ -198,32 +197,32 @@ void DrawArmB()
     glColor3f(1.0f,0.0f,0.0f);
     glBegin(GL_QUADS);
 
-    //画朝前的面
+    //鐢绘湞鍓嶇殑闈?
     glVertex3f(-0.2f, -0.56f, 0.21f);
     glVertex3f( 0.2f, -0.56f, 0.21f);
     glVertex3f( 0.2f,-2.0f, 0.21f);
     glVertex3f(-0.2f,-2.0f, 0.21f);
-    //画朝左的面
+    //鐢绘湞宸︾殑闈?
     glVertex3f( 0.2f, -0.56f, 0.21f);
     glVertex3f( 0.2f, -0.56f,-0.21f);
     glVertex3f( 0.2f,-2.0f,-0.21f);
     glVertex3f( 0.2f,-2.0f, 0.21f);
-    //画朝后的面
+    //鐢绘湞鍚庣殑闈?
     glVertex3f( 0.2f, -0.56f,-0.21f);
     glVertex3f(-0.2f, -0.56f,-0.21f);
     glVertex3f(-0.2f,-2.0f,-0.21f);
     glVertex3f( 0.2f,-2.0f,-0.21f);
-    //画朝右的面
+    //鐢绘湞鍙崇殑闈?
     glVertex3f(-0.2f, -0.56f, 0.21f);
     glVertex3f(-0.2f, -0.56f,-0.21f);
     glVertex3f(-0.2f,-2.0f,-0.21f);
     glVertex3f(-0.2f,-2.0f, 0.21f);
-    //画朝上的面
+    //鐢绘湞涓婄殑闈?
     glVertex3f( 0.2f, -0.56f, 0.21f);
     glVertex3f( 0.2f, -0.56f,-0.21f);
     glVertex3f(-0.2f, -0.56f,-0.21f);
     glVertex3f(-0.2f, -0.56f, 0.21f);
-    //画朝下的面
+    //鐢绘湞涓嬬殑闈?
     glVertex3f(-0.2f,-2.0f, 0.25f);
     glVertex3f( 0.2f,-2.0f, 0.25f);
     glVertex3f( 0.2f,-2.0f,-0.25f);
@@ -238,32 +237,32 @@ void DrawLegA()
     glVertex3f( 0.3f,0.25f, 0.0f);
     glVertex3f( 0.3f,-1.5f, 0.0f);
     glVertex3f(-0.3f,-1.5f, 0.0f);
-    //画朝前的面
+    //鐢绘湞鍓嶇殑闈?
     glVertex3f(-0.3f,0.25f, 0.255f);
     glVertex3f( 0.3f,0.25f, 0.255f);
     glVertex3f( 0.3f,-1.5f, 0.255f);
     glVertex3f(-0.3f,-1.5f, 0.255f);
-    //画朝左的面
+    //鐢绘湞宸︾殑闈?
     glVertex3f( 0.3f,0.25f, 0.255f);
     glVertex3f( 0.3f,0.25f,-0.255f);
     glVertex3f( 0.3f,-1.5f,-0.255f);
     glVertex3f( 0.3f,-1.5f, 0.255f);
-    //画朝后的面
+    //鐢绘湞鍚庣殑闈?
     glVertex3f( 0.3f,0.25f,-0.255f);
     glVertex3f(-0.3f,0.25f,-0.255f);
     glVertex3f(-0.3f,-1.5f,-0.255f);
     glVertex3f( 0.3f,-1.5f,-0.255f);
-    //画朝右的面
+    //鐢绘湞鍙崇殑闈?
     glVertex3f(-0.3f,0.25f, 0.255f);
     glVertex3f(-0.3f,0.25f,-0.255f);
     glVertex3f(-0.3f,-1.5f,-0.255f);
     glVertex3f(-0.3f,-1.5f, 0.255f);
-    //画朝上的面
+    //鐢绘湞涓婄殑闈?
     glVertex3f( 0.3f,0.25f, 0.255f);
     glVertex3f( 0.3f,0.25f,-0.255f);
     glVertex3f(-0.3f,0.25f,-0.255f);
     glVertex3f(-0.3f,0.25f, 0.255f);
-    //画朝下的面
+    //鐢绘湞涓嬬殑闈?
     glVertex3f( 0.3f,-1.5f, 0.255f);
     glVertex3f( 0.3f,-1.5f,-0.255f);
     glVertex3f(-0.3f,-1.5f,-0.255f);
@@ -279,32 +278,32 @@ void DrawLegB()
     glVertex3f( 0.3f,-1.2f, 0.0f);
     glVertex3f( 0.3f,-3.25f, 0.0f);
     glVertex3f(-0.3f,-3.25f, 0.0f);
-    //画朝前的面
+    //鐢绘湞鍓嶇殑闈?
     glVertex3f(-0.3f,-1.2f, 0.255f);
     glVertex3f( 0.3f,-1.2f, 0.255f);
     glVertex3f( 0.3f,-3.25f, 0.255f);
     glVertex3f(-0.3f,-3.25f, 0.255f);
-    //画朝左的面
+    //鐢绘湞宸︾殑闈?
     glVertex3f( 0.3f,-1.2f, 0.255f);
     glVertex3f( 0.3f,-1.2f,-0.255f);
     glVertex3f( 0.3f,-3.25f,-0.255f);
     glVertex3f( 0.3f,-3.25f, 0.255f);
-    //画朝后的面
+    //鐢绘湞鍚庣殑闈?
     glVertex3f( 0.3f,-1.2f,-0.255f);
     glVertex3f(-0.3f,-1.2f,-0.255f);
     glVertex3f(-0.3f,-3.25f,-0.255f);
     glVertex3f( 0.3f,-3.25f,-0.255f);
-    //画朝右的面
+    //鐢绘湞鍙崇殑闈?
     glVertex3f(-0.3f,-1.2f, 0.255f);
     glVertex3f(-0.3f,-1.2f,-0.255f);
     glVertex3f(-0.3f,-3.25f,-0.255f);
     glVertex3f(-0.3f,-3.25f, 0.255f);
-    //画朝上的面
+    //鐢绘湞涓婄殑闈?
     glVertex3f( 0.3f,-1.2f, 0.255f);
     glVertex3f( 0.3f,-1.2f,-0.255f);
     glVertex3f(-0.3f,-1.2f,-0.255f);
     glVertex3f(-0.3f,-1.2f, 0.255f);
-    //画朝下的面
+    //鐢绘湞涓嬬殑闈?
     glVertex3f( 0.3f,-3.25f, 0.255f);
     glVertex3f( 0.3f,-3.25f,-0.255f);
     glVertex3f(-0.3f,-3.25f,-0.255f);
@@ -317,7 +316,7 @@ void DrawHead()
     glColor3f(0.0f,1.0f,1.0f);
     glBegin(GL_QUADS);
 
-    //画朝前的面
+    //鐢绘湞鍓嶇殑闈?
     glTexCoord2f(0.0f, 1.0f);
     glVertex3f(-0.6f, 0.6f, 0.35f);
     glTexCoord2f(1.0f, 1.0f);
@@ -326,27 +325,27 @@ void DrawHead()
     glVertex3f( 0.6f,-0.6f, 0.35f);
     glTexCoord2f(0.0f, 0.0f);
     glVertex3f(-0.6f,-0.6f, 0.35f);
-    //画朝左的面
+    //鐢绘湞宸︾殑闈?
     glVertex3f( 0.6f, 0.6f, 0.35f);
     glVertex3f( 0.6f, 0.6f,-0.35f);
     glVertex3f( 0.6f,-0.6f,-0.35f);
     glVertex3f( 0.6f,-0.6f, 0.35f);
-    //画朝后的面
+    //鐢绘湞鍚庣殑闈?
     glVertex3f( 0.6f, 0.6f,-0.35f);
     glVertex3f(-0.6f, 0.6f,-0.35f);
     glVertex3f(-0.6f,-0.6f,-0.35f);
     glVertex3f( 0.6f,-0.6f,-0.35f);
-    //画朝右的面
+    //鐢绘湞鍙崇殑闈?
     glVertex3f(-0.6f, 0.6f, 0.35f);
     glVertex3f(-0.6f, 0.6f,-0.35f);
     glVertex3f(-0.6f,-0.6f,-0.35f);
     glVertex3f(-0.6f,-0.6f, 0.35f);
-    //画朝上的面
+    //鐢绘湞涓婄殑闈?
     glVertex3f( 0.6f, 0.6f, 0.35f);
     glVertex3f( 0.6f, 0.6f,-0.35f);
     glVertex3f(-0.6f, 0.6f,-0.35f);
     glVertex3f(-0.6f, 0.6f, 0.35f);
-    //画朝下的面
+    //鐢绘湞涓嬬殑闈?
     glVertex3f( 0.6f,-0.6f, 0.35f);
     glVertex3f( 0.6f,-0.6f,-0.35f);
     glVertex3f(-0.6f,-0.6f,-0.35f);
@@ -358,32 +357,32 @@ void DrawNick()
     glColor3f(0.0f,1.0f,1.0f);
     glBegin(GL_QUADS);
 
-    //画朝前的面
+    //鐢绘湞鍓嶇殑闈?
     glVertex3f(-0.2f, 0.1f, 0.25f);
     glVertex3f( 0.2f, 0.1f, 0.25f);
     glVertex3f( 0.2f,-0.1f, 0.25f);
     glVertex3f(-0.2f,-0.1f, 0.25f);
-    //画朝左的面
+    //鐢绘湞宸︾殑闈?
     glVertex3f( 0.2f, 0.1f, 0.25f);
     glVertex3f( 0.2f, 0.1f,-0.25f);
     glVertex3f( 0.2f,-0.1f,-0.25f);
     glVertex3f( 0.2f,-0.1f, 0.25f);
-    //画朝后的面
+    //鐢绘湞鍚庣殑闈?
     glVertex3f( 0.2f, 0.1f,-0.25f);
     glVertex3f(-0.2f, 0.1f,-0.25f);
     glVertex3f(-0.2f,-0.1f,-0.25f);
     glVertex3f( 0.2f,-0.1f,-0.25f);
-    //画朝右的面
+    //鐢绘湞鍙崇殑闈?
     glVertex3f(-0.2f, 0.1f, 0.25f);
     glVertex3f(-0.2f, 0.1f,-0.25f);
     glVertex3f(-0.2f,-0.1f,-0.25f);
     glVertex3f(-0.2f,-0.1f, 0.25f);
-    //画朝上的面
+    //鐢绘湞涓婄殑闈?
     glVertex3f( 0.2f, 0.1f, 0.25f);
     glVertex3f( 0.2f, 0.1f,-0.25f);
     glVertex3f(-0.2f, 0.1f,-0.25f);
     glVertex3f(-0.2f, 0.1f, 0.25f);
-    //画朝下的面
+    //鐢绘湞涓嬬殑闈?
     glVertex3f( 0.2f,-0.1f, 0.25f);
     glVertex3f( 0.2f,-0.1f,-0.25f);
     glVertex3f(-0.2f,-0.1f,-0.25f);
@@ -391,25 +390,25 @@ void DrawNick()
 
     glEnd();
 }
-//主画图程序
+//涓荤敾鍥剧▼搴?
 void DrawGLScene()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glLoadIdentity();//当前矩阵重置为单位矩阵
-    //一开始从右后方出现
-    glTranslatef(0.0f,0.0f,-40.0f);//将当前图像向z轴负方向移动
-    glRotatef(angle,0,1,0);//绕y轴不停旋转，造成移动的假象
-    glPushMatrix();//建立局部坐标系
+    glLoadIdentity();//褰撳墠鐭╅樀閲嶇疆涓哄崟浣嶇煩闃?
+    //涓€寮€濮嬩粠鍙冲悗鏂瑰嚭鐜?
+    glTranslatef(0.0f,0.0f,-40.0f);//灏嗗綋鍓嶅浘鍍忓悜z杞磋礋鏂瑰悜绉诲姩
+    glRotatef(angle,0,1,0);//缁晊杞翠笉鍋滄棆杞紝閫犳垚绉诲姩鐨勫亣璞?
+    glPushMatrix();//寤虹珛灞€閮ㄥ潗鏍囩郴
     glTranslatef(0.0f,0.0f,-20.0f);
-    glRotatef(-90,0,1,0);//保持脸正常朝向
+    glRotatef(-90,0,1,0);//淇濇寔鑴告甯告湞鍚?
     glPushMatrix();
 
-    glTranslatef(0.0f,1.2f,0.0f);//沿y轴正方向
+    glTranslatef(0.0f,1.2f,0.0f);//娌縴杞存鏂瑰悜
     glPushMatrix();
     glTranslatef(0.8,0.0,0.0);
     glTranslatef(0.0,-0.4,0.0);
-    glRotatef(angleOfLeftArmA,-1,0,0);//胳膊不断前后移动
-    //画胳膊的两段
+    glRotatef(angleOfLeftArmA,-1,0,0);//鑳宠唺涓嶆柇鍓嶅悗绉诲姩
+    //鐢昏兂鑶婄殑涓ゆ
     DrawArmA();
     glPushMatrix();
     glTranslatef(0.0,-0.37,0);
@@ -418,7 +417,7 @@ void DrawGLScene()
     DrawArmB();
     glPopMatrix();
     glPopMatrix();
-    glPopMatrix();//恢复全局坐标系
+    glPopMatrix();//鎭㈠鍏ㄥ眬鍧愭爣绯?
 
     glPushMatrix();
     glTranslatef(-0.8,0.0,0.0);
@@ -433,7 +432,7 @@ void DrawGLScene()
     glPopMatrix();
     glPopMatrix();
     glPopMatrix();
-    //返回肩膀
+    //杩斿洖鑲╄唨
     glPushMatrix();
     glTranslatef(0.0,0.3,0.0);
     glPushMatrix();
@@ -444,7 +443,7 @@ void DrawGLScene()
     glPopMatrix();
     DrawShoulder();
     glPopMatrix();
-    //返回身体
+    //杩斿洖韬綋
     glPushMatrix();
     glTranslatef(0.0f,-1.2f,0.0f);
     glPushMatrix();
@@ -480,29 +479,29 @@ void DrawGLScene()
     DrawBody();
     glPopMatrix();
     glPopMatrix();
-    //左右胳膊运动幅度保持一致
+    //宸﹀彸鑳宠唺杩愬姩骞呭害淇濇寔涓€鑷?
     angleOfLeftArmA+=a;
     angleOfRightArmA-=a;
 
-    //在主胳膊在身体上方时小胳膊的运动情况
+    //鍦ㄤ富鑳宠唺鍦ㄨ韩浣撲笂鏂规椂灏忚兂鑶婄殑杩愬姩鎯呭喌
     if(angleOfLeftArmA>0)
     {
-        //如果在向上运动的话
+        //濡傛灉鍦ㄥ悜涓婅繍鍔ㄧ殑璇?
         if(a>0)
         {
-            //小胳膊会上摆一定角度后保持
+            //灏忚兂鑶婁細涓婃憜涓€瀹氳搴﹀悗淇濇寔
             if(angleOfLeftArmB<45)
                 angleOfLeftArmB+=a;
             else angleOfLeftArmB=45;
         }
-        else//在向下运动的话小胳膊会下摆直至与主胳膊平齐
+        else//鍦ㄥ悜涓嬭繍鍔ㄧ殑璇濆皬鑳宠唺浼氫笅鎽嗙洿鑷充笌涓昏兂鑶婂钩榻?
             if(angleOfLeftArmB>0)
                 angleOfLeftArmB+=a;
     }
-    else//身体下方时与主胳膊运动情况相同
+    else//韬綋涓嬫柟鏃朵笌涓昏兂鑶婅繍鍔ㄦ儏鍐电浉鍚?
         angleOfLeftArmB=0;
 
-    //对右胳膊同理，不过a<0时代表向上运动
+    //瀵瑰彸鑳宠唺鍚岀悊锛屼笉杩嘺<0鏃朵唬琛ㄥ悜涓婅繍鍔?
     if(angleOfRightArmA>0)
     {
         if(a<0)
@@ -516,11 +515,11 @@ void DrawGLScene()
     }
     else angleOfRightArmB=0;
 
-    //左右腿运动幅度保持一致且与胳膊相对相反
+    //宸﹀彸鑵胯繍鍔ㄥ箙搴︿繚鎸佷竴鑷翠笖涓庤兂鑶婄浉瀵圭浉鍙?
     angleOfLeftLegA-=b;
     angleOfRightLegA+=b;
 
-    //在身体后方时小腿微微屈腿
+    //鍦ㄨ韩浣撳悗鏂规椂灏忚吙寰井灞堣吙
     if(angleOfRightLegA<0)
         angleOfRightLegB=-15;
     else
@@ -530,9 +529,9 @@ void DrawGLScene()
     else
         angleOfLeftLegB=0;
 
-    angle+=0.1;//整体绕y轴转的速度
+    angle+=0.1;//鏁翠綋缁晊杞磋浆鐨勯€熷害
 
-    //若运动到了最大幅度时，开始反向运动
+    //鑻ヨ繍鍔ㄥ埌浜嗘渶澶у箙搴︽椂锛屽紑濮嬪弽鍚戣繍鍔?
     if(angleOfLeftArmA>50)
     {
         a=-0.4;
@@ -546,16 +545,16 @@ void DrawGLScene()
     glutSwapBuffers();
 }
 
-//接收到按键时的响应程序
+//鎺ユ敹鍒版寜閿椂鐨勫搷搴旂▼搴?
 void keyPressed(unsigned char key, int x, int y)
 {
-    //短暂休眠，可以更加仔细观察运动情况
+    //鐭殏浼戠湢锛屽彲浠ユ洿鍔犱粩缁嗚瀵熻繍鍔ㄦ儏鍐?
     usleep(100000);
-    //如果是'Esc'就关闭窗口
+    //濡傛灉鏄?Esc'灏卞叧闂獥鍙?
     if (key == ESCAPE)
     {
         glutDestroyWindow(window);
-        //正常结束程序
+        //姝ｅ父缁撴潫绋嬪簭
         exit(0);
     }
 }
@@ -565,25 +564,25 @@ int main(int argc, char **argv)
 
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH);
-    //初始化屏幕大小
+    //鍒濆鍖栧睆骞曞ぇ灏?
     glutInitWindowSize(640, 480);
-    //屏幕从左上角开始
+    //灞忓箷浠庡乏涓婅寮€濮?
     glutInitWindowPosition(0, 0);
-    //打开屏幕
+    //鎵撳紑灞忓箷
     window = glutCreateWindow("STICK PERSON");
-    //设置画图函数
+    //璁剧疆鐢诲浘鍑芥暟
     glutDisplayFunc(&DrawGLScene);
-    //全屏
+    //鍏ㄥ睆
     glutFullScreen();
-    //全屏之后重新画图
+    //鍏ㄥ睆涔嬪悗閲嶆柊鐢诲浘
     glutIdleFunc(&DrawGLScene);
-    //设置全屏后大小变换函数
+    //璁剧疆鍏ㄥ睆鍚庡ぇ灏忓彉鎹㈠嚱鏁?
     glutReshapeFunc(&ReSizeGLScene);
-    //设置监听键盘功能
+    //璁剧疆鐩戝惉閿洏鍔熻兘
     glutKeyboardFunc(&keyPressed);
-    //初始化屏幕
+    //鍒濆鍖栧睆骞?
     InitGL(640, 480);
-    //开始循环执行
+    //寮€濮嬪惊鐜墽琛?
     glutMainLoop();
     return 1;
 }
